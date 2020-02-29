2b732a6e8fc82f61c77b0431c1e855fb99ed98bf
jcsesznegi <jcsesznegi@gmail.com>
2020-02-29 21:30:39 +0900

    day 10 part 2 (finally)

--- 10/part2.py
+++ 10/part2.py
@@ -6,7 +6,7 @@ f = open(os.path.join(os.path.dirname(__file__), '../input/10/part1.txt'), 'r')
 
 
 def reverseLength(circle, position, length):
-    if (position + length < len(circle)):
+    if (position + length <= len(circle)):
         before = circle[:position]
         middle = circle[position:position + length]
         after = circle[position + length:]
@@ -24,11 +24,10 @@ def reverseLength(circle, position, length):
 def doHashRound(circle, lengths, position=0, skip=0):
     for length in lengths:
         circle = reverseLength(circle, position, length)
-
-        position = position + length + skip
-        if (position >= len(circle)):
-            position = position % len(circle)
         
+        position = position + length + skip
+        position = position % len(circle)
+
         skip = skip + 1
 
     return circle, position, skip
@@ -36,6 +35,7 @@ def doHashRound(circle, lengths, position=0, skip=0):
 def doKnotHash(circle, lengths):
     position = 0
     skip = 0
+
     for _ in range(64):
         result = doHashRound(circle, lengths, position, skip)
         circle = result[0]
@@ -44,14 +44,11 @@ def doKnotHash(circle, lengths):
 
     groups = [circle[i:i + 16] for i in range(0, len(circle), 16)]
     xorGroups = [reduce(lambda i, j: i ^ j, group) for group in groups]
-    print(xorGroups)
     hexValues = [format(value, '02x') for value in xorGroups]
-    print(hexValues)
     return ''.join(hexValues)
 
 def main():
     line = f.readline().rstrip()
-    line = '1,2,3'
     lengths = [ord(character) for character in list(line)] + [17, 31, 73, 47, 23]
     result = doKnotHash(list(range(256)), lengths)
 
